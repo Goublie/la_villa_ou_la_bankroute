e59f61f1ede6ca4416a955c7a424f4ffd2cfa79b
pablo-delaunay <pablo.delaunay@edu.esiee.fr>
2026-05-14 18:52:39 +0200

    Ajout de l'interface du menu. Le bouton Jouer lance une scène, le bouton option ne fait rien et quitter envoie un message dans la console.

---
+++ Assets/programmes/Menu_du_jeu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_du_jeu : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        // Code pour démarrer le jeu
+        Debug.Log("Le jeu commence !");
+        SceneManager.LoadScene("Arrivée_dans_le_jeu");
+    }
+
+    public void QuitGame()
+    {
+        // Code pour quitter le jeu
+        Debug.Log("Le jeu se ferme !");
+        Application.Quit();
+    }
+    
+}
