ef2b9c1bf13e3bec51c2668ebcb32d57203530b6
Lucas <lucas.erba@edu.esiee.fr>
2026-05-22 23:52:13 +0200

    Affichage des ressources de base du jeu. L'affichage se fait dans la taskBar et est responsive. De même pour l'onglet banque.

--- code/unity/Assets/programmes/GameData.cs
+++ code/unity/Assets/programmes/GameData.cs
@@ -7,4 +7,14 @@ public class GameData : ScriptableObject
     public int argent = 1000;
     public int energie = 100;
     public int santeMentale = 100;
+
+
+
+    public void Start()
+    {
+        argent = 1000;
+        energie = 100;
+        santeMentale = 100;
+    }
+
 }
---
+++ code/unity/Assets/programmes/HUDManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro; 
+
+public class HUDManager : MonoBehaviour
+{
+
+    public GameData gameData; // Référence vers ton ScriptableObject
+
+    public TextMeshProUGUI texteArgent;
+    public TextMeshProUGUI texteEnergie;
+    public TextMeshProUGUI texteSanteMentale;
+
+    void Start()
+    {
+        ActualiserAffichage();
+    }
+
+
+    // Fonction qui rafraîchit l'interface à l'écran
+    public void ActualiserAffichage()
+    {
+        if (gameData == null) return;
+
+        if (texteArgent != null) 
+            texteArgent.text = gameData.argent.ToString() + " €";
+
+        if (texteEnergie != null) 
+            texteEnergie.text =  gameData.energie.ToString() + "%";
+
+        if (texteSanteMentale != null) 
+            texteSanteMentale.text = gameData.santeMentale.ToString() + "/100";
+    }
+}
