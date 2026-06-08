8a776ac627a5522fc6c90a1743b3ee6af810e493
Goublie <lu.le08120@gmail.com>
2026-06-08 15:19:00 +0200

    Le GameData se réinitialise à chaques lancement de jeu.

--- code/unity/Assets/programmes/GameData.cs
+++ code/unity/Assets/programmes/GameData.cs
@@ -1,10 +1,27 @@
 // Contient les données du jeu //
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "GameData", menuName = "Scriptable Objects/GameData")]
 public class GameData : ScriptableObject
 {
-    public int argent = 1000;
-    public int energie = 100;
-    public int santeMentale = 100;
+    public int salaire = 0;
+    public int energie = 100; 
+    public int santeMentale = 100; 
+    public int nombreMoisPasses = 0; 
+    public List<Investissement> investissements = new List<Investissement>(); // Liste des investissements du joueur
+    
+    /// <summary>
+    /// Réinitialise toutes les données de jeu à leur état par défaut.
+    /// Appelé lors du démarrage d'une nouvelle partie.
+    /// </summary>
+    public void ResetData()
+    {
+        salaire = 0;
+        energie = 100;
+        santeMentale = 100;
+        nombreMoisPasses = 0;
+        investissements.Clear();
+        
+    }
 }
